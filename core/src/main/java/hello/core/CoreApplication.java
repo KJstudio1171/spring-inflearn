1c275ac4b45b50932cac776ecbf8b264b9fe986f
KJstudio1171 <june1171@naver.com>
2022-11-11 00:17:59 +0900

    core

--- core/src/main/java/hello/core/CoreApplication.java
+++ core/src/main/java/hello/core/CoreApplication.java
@@ -1,13 +1,16 @@
 package hello.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class CoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
-
 }
